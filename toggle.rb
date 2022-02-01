9cfdfb1427c8be976f88535dc25785f5a08b3fce
Mohamed Ali <12maali27@gmail.com>
2022-02-01 02:15:32 +0000

    toggles from player 1 to player 2

---
+++ toggle.rb
@@ -0,0 +1,9 @@
+module Toggle
+    def toggle_player
+        if self.current_player == player1
+            self.current_player = player2
+        else
+            self.current_player = player1
+        end
+    end
+end
